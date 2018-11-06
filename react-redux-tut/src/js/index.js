335e1502557e2973338723f25b789ac42bffd884
Janell Anderson <andersonjanell@gmail.com>
2018-11-06 12:35:02 -0500

    Added components in js file and a new app.js file

--- react-redux-tut/src/js/index.js
+++ react-redux-tut/src/js/index.js
@@ -1,5 +1,22 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import App from "./components/App";
+
+render(
+    <Provider store={store}>
+    <App />
+    </Provider>,
+    document.getElementById("app")
+)
+
+// Provider wraps up your React app and makes it aware of the entire Redux's store.
+
+/* OLD CODE
 import store from "../js/store/index";
 import { addArticle } from "../js/actions/index";
 
 window.store = store;
 window.addArticle = addArticle;
+*/
