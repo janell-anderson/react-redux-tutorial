fecc78518c4d22e890d945e4a1e42841dce9e731
Janell Anderson <andersonjanell@gmail.com>
2018-11-09 15:06:21 -0500

    Still have the same error from day 3. Will look more into understanding of React as a whole and come back to this.

--- react-redux-tut/src/js/index.js
+++ react-redux-tut/src/js/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+// import ReactDOM from 'react-dom'
 import { Provider } from "react-redux";
 import store from "./store/index";
 import App from "./components/App";
 
+// const render = ReactDOM.render;
 render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("app")
 );
+
 
 // Provider wraps up your React app and makes it aware of the entire Redux's store.
 
